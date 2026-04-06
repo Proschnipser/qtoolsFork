1e833022c9c0493f0b4ecb114d31d0266adb2c7d
Proschnipser <156222735+Proschnipser@users.noreply.github.com>
2026-04-06 02:26:23 +0200

    Update read_tbl.py

--- Skripte/read_tbl.py
+++ Skripte/read_tbl.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import sys
+from pathlib import Path
 from io import StringIO
+from collections import defaultdict
 
 COLS = [
     'target_name', 'target_accession', 'query_name', 'query_accession',
@@ -9,16 +12,27 @@ COLS = [
     'description'
 ]
 
-with open('/data/joscha/output/hmmer_hits_long/GCF_020745735.1_sHemOce1.pat.X.cur._genomic_chunkedTANGO1.tbl') as f:
-    content = ''.join(l for l in f if not l.startswith('#'))
+directory="/data/joscha/output/hmmer_hits_long/" #sys.argv[1]
+dfdict=defaultdict(dict)
+for filepath in Path(directory):
+    with open(file) as f:
+        content = ''.join(l for l in f if not l.startswith('#'))
 
-df = pd.read_csv(StringIO(content), sep=r'\s+', header=None, names=COLS, usecols=range(19))
+    df = pd.read_csv(StringIO(content), sep=r'\s+', header=None, names=COLS, usecols=range(19))
 
-# Cast numeric columns
-for col in ['e_value', 'score', 'bias', 'e_value_best', 'score_best', 'bias_best', 'exp']:
-    df[col] = df[col].astype(float)
-for col in ['reg', 'clu', 'ov', 'env', 'dom', 'rep', 'inc']:
-    df[col] = df[col].astype(int)
+    # Cast numeric columns
+    for col in ['e_value', 'score', 'bias', 'e_value_best', 'score_best', 'bias_best', 'exp']:
+        df[col] = df[col].astype(float)
+    for col in ['reg', 'clu', 'ov', 'env', 'dom', 'rep', 'inc']:
+        df[col] = df[col].astype(int)
 
-print(df.head())
-print(df)
+    print(df.head())
+    splitname=filepath.split("_")
+    dfdict[splitname[1]][splitname[-1]]=df
+
+
+
+for k1,v1 in dfdict.items():
+    for k2, v2 in v1.items():
+        for i, r in v2.iterrows():
+            r[]
