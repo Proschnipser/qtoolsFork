fe6d8b8e29b33d1177930c68059d4d71b0894d92
Proschnipser <156222735+Proschnipser@users.noreply.github.com>
2026-04-07 10:10:02 +0200

    Update read_tbl.py

--- Skripte/read_tbl.py
+++ Skripte/read_tbl.py
@@ -59,7 +59,6 @@ for k1,v1 in dfdict.items(): #iterate over genomes
                 break
             print(not r["target_name"] in orfdict or r["e_value"] < orfdict[r["target_name"]][4])
             print(not r["target_name"] in orfdict)
-            print(r["e_value"] < orfdict[r["target_name"]][4])
             if r["length"] > 99 and (not r["target_name"] in orfdict or r["e_value"] < orfdict[r["target_name"]][4]):
                 print(list(r))
                 orfdict[r["target_name"]]=list(r)
