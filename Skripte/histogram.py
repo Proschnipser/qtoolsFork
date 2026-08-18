440d63d4ffc80d5199f82e0ce7fedb185a19cb77
Proschnipser <156222735+Proschnipser@users.noreply.github.com>
2026-08-18 13:26:14 +0200

    Update histogram.py

--- Skripte/histogram.py
+++ Skripte/histogram.py
@@ -155,7 +155,6 @@ def wald_tests_on_tree(tree, leaf_values):
             statistics=[]
             for i in range(x[0].shape[0]): #iterate over neurons
                 for j in range(x[0].shape[1]): #iterate over weighted distances
-                    print(x[:][i].shape)
                     print(i,j)
                     statistic, p_value = wald_test_two_groups([x[c][i][j] for c in range(x.shape[0])], [x[c][i][j] for c in range(x.shape[0])])
                     p_values.append(p_value)
